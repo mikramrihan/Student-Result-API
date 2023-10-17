22ec44f2959e5c0dcc4b8ccd456af6419d141c8a
MOHD RIHAN <66411637+mikramrihan@users.noreply.github.com>
2023-10-17 09:04:54 +0530

    Update Studentdetail.java

--- Studentresult/src/main/java/com/exampleResult/Studentresult/service/Studentdetail.java
+++ Studentresult/src/main/java/com/exampleResult/Studentresult/service/Studentdetail.java
@@ -3,14 +3,13 @@ package com.exampleResult.Studentresult.service;
 import com.exampleResult.Studentresult.Entity.Student;
 
 import java.util.List;
-
+// R I H A N
 public interface Studentdetail {
     List<Student> findall();
     Student find(int id);
     int findid(int id);
     Student save(Student student);
     int findlatestid();
-////    void deletebyId(int id);
     public List<Student> getquery(int studentID, String studentdob);
 
 }

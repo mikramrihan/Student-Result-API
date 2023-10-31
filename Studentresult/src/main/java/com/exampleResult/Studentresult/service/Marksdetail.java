2d7ba7da19d9c90a31df4cc5f430eeadc9ef1e1f
MOHD RIHAN <66411637+mikramrihan@users.noreply.github.com>
2023-10-31 10:25:34 +0530

    Update Marksdetail.java
    Just added some comments and main function

--- Studentresult/src/main/java/com/exampleResult/Studentresult/service/Marksdetail.java
+++ Studentresult/src/main/java/com/exampleResult/Studentresult/service/Marksdetail.java
@@ -1,15 +1,13 @@
 package com.exampleResult.Studentresult.service;
-
 import com.exampleResult.Studentresult.Entity.Marks;
 import com.exampleResult.Studentresult.Entity.Student;
-
 import java.util.List;
-
+// RIHAN 
+// Just some function through which i can connect this to there
 public interface Marksdetail {
     List<Marks> findall();
     Marks find(int id);
     List<Marks> findbyid(int id);
     Marks save(Marks marks);
-
     List<Marks> findbystudentid(int studentid);
 }
